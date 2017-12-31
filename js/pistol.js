7253c5e206ed916ced718dd07e3f2e7dd798ab79
Dohrny <dohrny@gmail.com>
2017-12-31 15:35:56 -0600

    forgot to save some files

--- js/pistol.js
+++ js/pistol.js
@@ -1,14 +1,1 @@
-class Pistol extends Phaser.Weapon {
-    constructor(game, parent) {
-        super(game, game.plugins);
-        this.bulletKillType = Phaser.Weapon.KILL_DISTANCE;
-        this.bulletKillDistance = 720;
-        this.bulletSpeed = 500;
-        this.fireRate = 250;
-    }
-    init(someVar) {
-        this.someVar = someVar;
-    }
-}
-export { Pistol };
 //# sourceMappingURL=pistol.js.map
--- ts/pistol.ts
+++ ts/pistol.ts
@@ -1,19 +1,3 @@
-
-class Pistol extends Phaser.Weapon {
-    someVar: string
-
-    constructor(game: Phaser.Game, parent: Phaser.PluginManager) {
-        super(game, game.plugins)
-        this.bulletKillType = Phaser.Weapon.KILL_DISTANCE
-        this.bulletKillDistance = 720
-        this.bulletSpeed = 500
-        this.fireRate = 250
-    }
-
-    init(someVar: string) {
-        this.someVar = someVar
-    }
-}
-
-export { Pistol }
+import {game} from './game'
+import { Weapon } from 'phaser-ce';
 
