991f9b87a4be3ac46f85bf52ebf5a0e8ec30c543
Dohrny <dohrny@gmail.com>
2018-01-12 23:06:11 -0600

    part of something bigger now

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-var express = require('express')
-var app = express()
-var server = require('http').Server(app)
-var io = require('socket.io')(server)
+const express = require('express')
+const app = module.exports = express()
+//var server = require('http').Server(app)
+//var io = require('socket.io')(server)
+const path = require('path')
 
 
 //app.use('/css',express.static(__dirname + '/css'));
+//app.set('views', express.static(__dirname))
 app.use('/js',express.static(__dirname + '/js'));
 app.use('/assets',express.static(__dirname + '/assets'));
 app.use('/css', express.static(__dirname + '/css'))
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html');
+app.get('/', (req,res) => {
+    //res.sendFile(__dirname+'/index.html');
+    res.send('hi from 2dbr')
 });
 
-io.on('connection', function(socket) {
-    console.log('user connected...id: ' + socket.id)
-    socket.on('disconnect', function() {
-        console.log('user disconnected')
-    })
-})
+// io.on('connection', function(socket) {
+//     console.log('user connected...id: ' + socket.id)
+//     socket.on('disconnect', function() {
+//         console.log('user disconnected')
+//     })
+// })
 
-server.listen(8081,function(){ // Listens to port 8081
-    console.log('Listening on ' + server.address().port);
-})
+// server.listen(8081,function(){ // Listens to port 8081
+//     console.log('Listening on ' + server.address().port);
+// })
